11a2e43b946459874acb9a2afe2d038456a6d2fc
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-10-29 10:06:13 +0600

    change the name of section sort function

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -8,7 +8,7 @@ void printSortedArray(int *arr,int n){
     cout<<endl;
 }
 
-void bubbleSort(int arr[],int n){
+void selectionSort(int arr[],int n){
     for(int i=0;i<n-1;i++){
         int minIndex=i;
         for(int j=i+1;j<n;j++){
@@ -24,7 +24,7 @@ void bubbleSort(int arr[],int n){
 int main(){
    int arr[]={5,4,1,3,2};
    int n =sizeof(arr)/sizeof(int);
- bubbleSort(arr,n);
+ selectionSort(arr,n);
 
     return 0;
 }
